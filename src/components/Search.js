8818271af8782e4d96b6bc9fa7878c47b0ba3a4b
David Kerr <kerrky@gmail.com>
2024-05-23 18:51:30 -0400

    Updated files

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,20 +1,17 @@
-
-function Search({ onSearchText, searchText}) {
-
-
-return(
-  <div className="searchbar">
-    <label htmlFor="search">Search Movies: </label>
-    <input
-    type="text"
-    id="search"
-    className="search-input"
-    placeholder="Search by Name, Actor or Director"
-    onChange={onSearchText}
-    value={searchText}
-    />
-  </div>
-)
+function Search({ onSearchText, searchText }) {
+  return (
+    <div className="searchbar">
+      <label htmlFor="search">Search Movies: </label>
+      <input
+        type="text"
+        id="search"
+        className="search-input"
+        placeholder="Search by Name, Actor or Director"
+        onChange={onSearchText}
+        value={searchText}
+      />
+    </div>
+  );
 }
 
 export default Search;
--- src/pages/NewMovieForm.js
+++ src/pages/NewMovieForm.js
@@ -1,9 +1,8 @@
 import { useState } from "react";
 import { useOutletContext } from "react-router-dom";
 
-function NewMovieForm(){
-
-  const [ formData, setFormData ] = useState({
+function NewMovieForm() {
+  const [formData, setFormData] = useState({
     name: "",
     year: "",
     actor: "",
@@ -12,16 +11,15 @@ function NewMovieForm(){
     description: "",
     image: "",
     favorite: false,
-    likes: 0
-  })
+    likes: 0,
+  });
 
- 
-  const {addNewMovie} = useOutletContext()
+  const { addNewMovie } = useOutletContext();
 
   function handleNewMovieSubmit(event) {
-    event.preventDefault()
-    const movieYear = parseInt(formData.year)
-    addNewMovie({...formData, year: movieYear})
+    event.preventDefault();
+    const movieYear = parseInt(formData.year);
+    addNewMovie({ ...formData, year: movieYear });
 
     setFormData({
       name: "",
@@ -30,94 +28,93 @@ function NewMovieForm(){
       director: "",
       genre: "",
       description: "",
-      image: ""
-    })
-    
+      image: "",
+    });
   }
 
   function handleNewMovieFormData(event) {
-    setFormData({...formData, [event.target.name]: event.target.value})
+    setFormData({ ...formData, [event.target.name]: event.target.value });
   }
-
-
-  return(
-   <div className="center-form">
-    <form className="submit-form" onSubmit={handleNewMovieSubmit}>
-      <label>Movie Name: </label>
-      <input 
-      type="text" 
-      name="name" 
-      placeholder="Add Movie Name" 
-      value={formData.name} 
-      onChange={handleNewMovieFormData} 
-      required
-      />
-      <br/>
-      <label>Release Year: </label>
-      <input 
-      type="number" 
-      name="year" 
-      placeholder="Add Movie Year" 
-      value={formData.year} 
-      onChange={handleNewMovieFormData} 
-      required
-      />
-      <br/>
-      <label>Actors: </label>
-      <input 
-      type="text" 
-      name="actor" 
-      placeholder="Add Actor" 
-      value={formData.actor} 
-      onChange={handleNewMovieFormData} 
-      required
-      />
-      <br/>
-      <label>Director: </label>
-      <input 
-      type="text" 
-      name="director" 
-      placeholder="Add Director" 
-      value={formData.director} 
-      onChange={handleNewMovieFormData} 
-      required
-      />
-      <br/>
-      <label>Genre: </label>
-      <input 
-      type="text" 
-      name="genre" 
-      placeholder="Add Genre" 
-      value={formData.genre} 
-      onChange={handleNewMovieFormData} 
-      required
-      />
-      <br/>
-      <label>Description: </label>
-      <textarea 
-      type="text" 
-      name="description" 
-      placeholder="Add Description" 
-      value={formData.description} 
-      onChange={handleNewMovieFormData} 
-      required
-      />
-      <br/>
-      <label>Movie Poster: </label>
-      <input 
-      type="text" 
-      name="image" 
-      placeholder="Add Movie Poster" 
-      value={formData.image} 
-      onChange={handleNewMovieFormData} 
-      required
-      />
-      <br/>
-      <button type="submit" className="submit-button">Add Movie</button>
-    </form>
-  </div> 
-  
-  )
+  console.log(formData);
+  return (
+    <div className="center-form">
+      <form className="submit-form" onSubmit={handleNewMovieSubmit}>
+        <label>Movie Name: </label>
+        <input
+          type="text"
+          name="name"
+          placeholder="Add Movie Name"
+          value={formData.name}
+          onChange={handleNewMovieFormData}
+          required
+        />
+        <br />
+        <label>Release Year: </label>
+        <input
+          type="number"
+          name="year"
+          placeholder="Add Movie Year"
+          value={formData.year}
+          onChange={handleNewMovieFormData}
+          required
+        />
+        <br />
+        <label>Actors: </label>
+        <input
+          type="text"
+          name="actor"
+          placeholder="Add Actor"
+          value={formData.actor}
+          onChange={handleNewMovieFormData}
+          required
+        />
+        <br />
+        <label>Director: </label>
+        <input
+          type="text"
+          name="director"
+          placeholder="Add Director"
+          value={formData.director}
+          onChange={handleNewMovieFormData}
+          required
+        />
+        <br />
+        <label>Genre: </label>
+        <input
+          type="text"
+          name="genre"
+          placeholder="Add Genre"
+          value={formData.genre}
+          onChange={handleNewMovieFormData}
+          required
+        />
+        <br />
+        <label>Description: </label>
+        <textarea
+          type="text"
+          name="description"
+          placeholder="Add Description"
+          value={formData.description}
+          onChange={handleNewMovieFormData}
+          required
+        />
+        <br />
+        <label>Movie Poster: </label>
+        <input
+          type="text"
+          name="image"
+          placeholder="Add Movie Poster"
+          value={formData.image}
+          onChange={handleNewMovieFormData}
+          required
+        />
+        <br />
+        <button type="submit" className="submit-button">
+          Add Movie
+        </button>
+      </form>
+    </div>
+  );
 }
 
 export default NewMovieForm;
