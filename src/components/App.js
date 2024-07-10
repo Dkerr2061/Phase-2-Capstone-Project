d4e3cf01ab997588ad01fe77748d69ba3c35d8b3
David Kerr <kerrky@gmail.com>
2024-07-10 00:33:00 -0400

    added vercel json

--- src/components/App.js
+++ src/components/App.js
@@ -3,40 +3,41 @@ import { useState, useEffect } from "react";
 import NavBar from "./NavBar";
 
 function App() {
-  const [ movies, setMovies ] = useState([])
-  
+  const [movies, setMovies] = useState([]);
+  console.log(movies);
   useEffect(() => {
-    fetch('http://localhost:3000/movieList')
-      .then(res => res.json())
-      .then(movieData => setMovies(movieData))
-  }, [])
+    fetch("http://localhost:3000/movieList")
+      .then((res) => res.json())
+      .then((movieData) => setMovies(movieData));
+  }, []);
 
   function addNewMovie(newMovie) {
-    fetch('http://localhost:3000/movieList', {
-      method: 'POST',
+    fetch("http://localhost:3000/movieList", {
+      method: "POST",
       headers: {
-        'Content-Type': 'Application/JSON',
-        "Accept": "application/json"
+        "Content-Type": "Application/JSON",
+        Accept: "application/json",
       },
-      body: JSON.stringify({...newMovie})
+      body: JSON.stringify({ ...newMovie }),
     })
-      .then(res => res.json())
-      .then(newMovieData => setMovies([...movies, newMovieData]))
+      .then((res) => res.json())
+      .then((newMovieData) => setMovies([...movies, newMovieData]));
   }
 
   function deleteMovie(id) {
     fetch(`http://localhost:3000/movieList/${id}`, {
-      method: 'DELETE'
-    })
-      .then(res => {
-        if(res.ok) {
-          setMovies((movies) => movies.filter( movie => {
-            return movie.id !== id
-          }))
-        } else {
-          alert("Error, cannot delete movie at this time!")
-        }
-      })
+      method: "DELETE",
+    }).then((res) => {
+      if (res.ok) {
+        setMovies((movies) =>
+          movies.filter((movie) => {
+            return movie.id !== id;
+          })
+        );
+      } else {
+        alert("Error, cannot delete movie at this time!");
+      }
+    });
   }
 
   function updateMovie(id, dataToUpdate) {
@@ -44,18 +45,22 @@ function App() {
       method: "PATCH",
       headers: {
         "Content-Type": "Application/JSON",
-        "Accept": "application/json"
+        Accept: "application/json",
       },
-      body: JSON.stringify(dataToUpdate)
+      body: JSON.stringify(dataToUpdate),
     })
-      .then(res => res.json())
-      .then(newData => setMovies(movies => movies.map(movie => {
-        if(movie.id === id) {
-          return newData
-        } else {
-          return movie
-        }
-      })))
+      .then((res) => res.json())
+      .then((newData) =>
+        setMovies((movies) =>
+          movies.map((movie) => {
+            if (movie.id === id) {
+              return newData;
+            } else {
+              return movie;
+            }
+          })
+        )
+      );
   }
 
   return (
@@ -63,14 +68,15 @@ function App() {
       <header className="App-header">
         <h1 className="website-name">FlixFix Junction</h1>
       </header>
-      <NavBar/>
-      <Outlet 
-      context={{ 
-        movies: movies, 
-        addNewMovie: addNewMovie, 
-        deleteMovie: deleteMovie,
-        updateMovie: updateMovie
-      }}/>
+      <NavBar />
+      <Outlet
+        context={{
+          movies: movies,
+          addNewMovie: addNewMovie,
+          deleteMovie: deleteMovie,
+          updateMovie: updateMovie,
+        }}
+      />
     </div>
   );
 }
